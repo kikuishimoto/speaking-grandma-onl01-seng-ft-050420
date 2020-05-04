461bc8e5dbc34a3752af14783faca54a619e3c16
kiku ishimoto <kikuish@gmail.com>
2020-05-04 21:24:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,11 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+  if phrase == phrase.lowercase
+    return "HUH?! SPEAK UP, SONNY"
+  elsif phrase == 'I LOVE YOU GRANDMA!'
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    return "NO, NOT SINCE 1938!"
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
