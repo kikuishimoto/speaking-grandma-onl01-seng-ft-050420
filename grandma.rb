180daa656b12dc5d248a7e1aca487c7331e9c36a
kiku ishimoto <kikuish@gmail.com>
2020-05-04 21:33:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,10 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.downcase
     puts "HUH?! SPEAK UP, SONNY"
-  elsif phrase == 'I LOVE YOU GRANDMA!'
-    puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
 end
 # Whatever you say to grandma, she should respond with
